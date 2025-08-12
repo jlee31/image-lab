0c02d0f04fb468a4c2b3d5313f68e3272694dcf4
Lee <Joseph.Lee@ec.gc.ca>
2025-08-12 15:45:01 -0400

    updated folder structure

--- gui/app_window.py
+++ gui/app_window.py
@@ -1,20 +1,38 @@
 '''
 This file is responsible for:
-
 Creating the Tkinter window and widgets (buttons, labels, canvas, etc.).
-
 Handling user interactions (button clicks, sliders, etc.).
-
 Displaying images (e.g., showing OpenCV processed images in the GUI).
-
 Calling image processing functions when needed.
-
 Managing the app’s main event loop.
-
 '''
 
-from imports import * 
+from utils.imports import * 
+from utils.utils import *
 
 class AppWindow:
-    pass
+    # * Initial Setup
+    def __init__(self):
+        pass
+
+
+    # * Tkinter Window Setup
+    def create_buttons(self):
+        pass
+
+    def create_effect_options(self):
+        pass
+
+    # * Image  
+
+    def reset_image(self):
+        pass
+
+    def undo_image(self):
+        pass
+
+    def redo_image(self):
+        pass
+
+    # * Image Editing / Functions 
 
