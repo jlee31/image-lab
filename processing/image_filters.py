7e7158ee5cf46098e96cdd35417479d91e77dd03
Lee <Joseph.Lee@ec.gc.ca>
2025-08-13 11:51:30 -0400

    brightness module

--- processing/image_filters.py
+++ processing/image_filters.py
@@ -10,11 +10,22 @@ Encapsulating specific image operations like grayscale conversion, edge detectio
 Keeping the image processing logic independent from the GUI.
 '''
 from utils.imports import * 
+from utils.utils import * 
+from PIL import ImageEnhance
 
 # all functions will return an adjusted image
 def adjust_brightness(image):
     print("Adjusting Brightness")
-    return image
+    if not check_image_loaded(image):
+            return
+    newImage = image.copy()
+    brightness_factor = simpledialog.askfloat("Input", "Enter Brightness Factor (0.0 to 2.0)", minvalue=0.0, maxvalue=2.0)
+    if brightness_factor:
+        image_pil = Image.fromarray(cv.cvtColor(newImage, cv.COLOR_BGR2RGB))
+        enhancer = ImageEnhance.Brightness(image_pil)
+        enhanced_image = enhancer.enhance(brightness_factor)
+        newImage = cv.cvtColor(np.array(enhanced_image), cv.COLOR_RGB2BGR)
+    return newImage
 
 def adjust_saturation(image):
     pass
